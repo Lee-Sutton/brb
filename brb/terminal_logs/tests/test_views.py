c7c27a838d102676b560c90b83315865f09e9c29
Lee Sutton <leesutton1@gmail.com>
2019-04-04 17:26:50 -0700

    check for log list create button

--- brb/terminal_logs/tests/test_views.py
+++ brb/terminal_logs/tests/test_views.py
@@ -2,10 +2,21 @@ import pytest
 from django.urls import reverse
 from django.test import Client
 
+from django.contrib.auth import get_user_model
+
 
 @pytest.mark.django_db
-def test_log_list(user, client: Client):
+def test_log_list(client: Client):
     """It should return a valid response"""
-    client.login(username=user.username, password=user.password)
-    response = client.get(reverse('terminal_logs:log_list'), follow=True)
+    username = 'testuser'
+    password = 'testpass'
+    User = get_user_model()
+    User.objects.create_user(username, password=password)
+    logged_in = client.login(username=username, password=password)
+
+    assert logged_in
+    response = client.get(reverse('terminal_logs:log_list'))
     assert response.status_code == 200
+
+    create_url = reverse('terminal_logs:create')
+    assert create_url in str(response.content)
