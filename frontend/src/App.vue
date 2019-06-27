e2f6efec487d52fbc561a5feb686402f6d3a2abb
Lee Sutton <leesutton1@gmail.com>
2019-06-27 10:51:23 -0700

    finished sidebar placeholder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,45 +10,17 @@
         <a href="#" class="list-group-item list-group-item-action bg-light">Handicap</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Short Game</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Long Game</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Find Courses</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Courses</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Find Friends</a>
       </div>
     </div>
     <!-- /#sidebar-wrapper -->
 
     <!-- Page Content -->
     <div id="page-content-wrapper">
+      <Navbar></Navbar>
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <div class="container-fluid">
+      <div class="container" id="router-view">
         <router-view></router-view>
       </div>
 
@@ -62,7 +34,9 @@
 <!-- Bootstrap core JavaScript -->
 <!-- Menu Toggle Script -->
 <script>
+  import Navbar from '@/components/Navbar'
   export default {
+    components: {Navbar},
     name: 'App'
   }
 </script>
@@ -70,6 +44,10 @@
 <style scoped>
   body {
     overflow-x: hidden;
+  }
+
+  #router-view {
+    margin-top: 5%;
   }
 
   #sidebar-wrapper {
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
     <b-navbar class="navbar navbar-expand-lg navbar-light bg-light" variant="info">
-      <button class="btn btn-primary" id="menu-toggle" @click="toggleSidebar">Toggle Menu</button>
-
-      <!--<b-navbar-brand href="#">Golf Buddy</b-navbar-brand>-->
+      <h2>Scores</h2>
 
       <b-collapse id="nav-collapse" is-nav>
         <!--FIXME replace this with a helper function-->
-        <template v-if="$store.state.isLoggedIn">
-          <b-navbar-nav>
-            <b-nav-item to="scores">Scores</b-nav-item>
-          </b-navbar-nav>
-        </template>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
 
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template slot="button-content"><em>User</em></template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <template v-if="$store.state.isLoggedIn">
+            <b-nav-form>
+              <b-nav-item>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Find a course"></b-form-input>
+              </b-nav-item>
+            </b-nav-form>
+
+            <b-navbar-nav>
+              <b-nav-item to="scores">Add Score</b-nav-item>
+              <b-nav-item to="scores">Handicap</b-nav-item>
+            </b-navbar-nav>
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template slot="button-content"><em>User</em></template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+          </template>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
